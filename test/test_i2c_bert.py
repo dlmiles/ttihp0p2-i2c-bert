ff70e5d94db84d36b7e3246d9c54418b4e3b45c0
Darryl L. Miles <darryl.miles@darrylmiles.org>
2024-11-02 18:14:48 +0000

    test/**

--- test/test_i2c_bert.py
+++ test/test_i2c_bert.py
@@ -1,32 +1,39 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: Copyright © 2024 Darryl Miles
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
 
 
 @cocotb.test()
-async def test_project(dut):
+async def test_i2c_bert(dut):
     dut._log.info("Start")
+
+    await Timer(40, units='us') # Show X states
+
+    dut.clk.value = 0
+    dut.ena.value = 0
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await Timer(40, units='us') # Show quiescent state
 
     # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
+    # ENA
+    dut.ena.value = 1
+    await ClockCycles(dut.clk, 4)
+
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 4)
 
-    dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
+
     dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
@@ -34,7 +41,7 @@ async def test_project(dut):
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    #assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
